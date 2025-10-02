590b5adbfce32d352034c73052c833406ceb75b1
Fabio Colombini <fabiorcolombini@gmail.com>
2025-10-02 20:10:11 -0300

    JUSTES

---
+++ src/Aula_6/Main_ArrayList.java
@@ -0,0 +1,42 @@
+package Aula_6;
+
+import java.util.ArrayList;
+
+public class Main_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> lstNomes = new ArrayList<>();
+        
+        lstNomes.add("Ramones");
+        lstNomes.add("Spider Man");
+        lstNomes.add("Darth Vader");
+        
+        /*if (lstNomes.contains("Spider Man")) {
+            
+        }*/
+        
+        //System.out.println(lstNomes.get(2));
+        
+        /*lstNomes.clear();
+        
+        if (lstNomes.isEmpty()) {
+            System.out.println("Voce precisa cadastrar um item");
+        }
+        else {
+            System.out.println("Tem item cadastrado");
+        }*/
+        
+        /*lstNomes.remove(1);
+        
+        System.out.println(lstNomes.get(1));*/
+        
+        //System.out.println(lstNomes.size());
+        
+        /*for (int i = 0; i < lstNomes.size(); i++) {
+            System.out.println(lstNomes.get(i));
+        }*/
+        
+        /*for (String nome : lstNomes) {
+            System.out.println(nome);
+        }*/
+    }
+}
---
+++ src/Aula_6/Main_Pessoa.java
@@ -0,0 +1,8 @@
+package Aula_6;
+
+public class Main_Pessoa {
+
+    public static void main(String[] args) {
+        
+    }
+}
