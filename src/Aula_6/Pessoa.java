f61c2f2679980368cab5622f2751f01972a67b08
Fabio Colombini <fabiorcolombini@gmail.com>
2025-10-02 19:57:16 -0300

    Aula 6

---
+++ src/Aula_6/Pessoa.java
@@ -0,0 +1,56 @@
+package Aula_6;
+
+import Aula_5.*;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private Livro livro;
+    private int diasDevolucao;
+    
+    public Pessoa() {
+        System.out.println("Objeto instanciado...");
+    }
+    
+    public Pessoa(String nome, int idade) {
+        this();
+        this.nome = nome;
+        this.idade = idade;
+    }
+    
+    public Pessoa(String nome, int idade, Livro livro, int diasDevolucao) {
+        this(nome, idade);
+        this.livro = livro;
+        this.diasDevolucao = diasDevolucao;
+    }
+    
+    public String getLivro() {
+        if (this.livro == null) {
+            return "***";
+        }
+        else {
+            return livro.getNome();
+        }
+    }
+    
+    public void emprestar(Livro livro) {
+        this.livro = livro;
+        this.diasDevolucao = 20;
+    }
+    
+    public void emprestar(Livro livro, int dias) {
+        this.livro = livro;
+        this.diasDevolucao = dias;
+    }
+    
+    public void imprimir() {
+        String msg = "\n==================="
+                    +"\nNome: "+nome
+                    +"\nIdade: "+idade
+                    +"\nLivro: "+livro.imprimir()
+                    +"\nDias Devolucao: "+diasDevolucao
+                    +"\n===================";
+        System.out.println(msg);
+        
+    }
+}
